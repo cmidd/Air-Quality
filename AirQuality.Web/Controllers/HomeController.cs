54064e70507d55723331e50137e97cf8acd120ed
corey-cb <corey@coldbananastudio.com>
2022-01-06 11:21:27 +0000

    Created project

---
+++ AirQuality.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using AirQuality.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirQuality.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOpenAqApiService _openAqApiService;
+
+        public HomeController(IOpenAqApiService openAqApiService)
+        {
+            _openAqApiService = openAqApiService;
+        }
+
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
